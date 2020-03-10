74bf28c2a5fe9198771f03d5a73bd6256c5d59b8
Joel Sprouse <joel.sprouse11@gmail.com>
2020-03-10 18:01:45 +0000

    Create header1.php

---
+++ includes/header1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="refresh" content="6">
+	<title><?php echo $page_title; ?></title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+	<div class="header1">
+		<a href="http://schoolwork-helpsite.github.io/swhs3/en/index.html" class="hidden">
+			<img src="http://schoolwork-helpsite.github.io/swhs3/media/logo.jpg">
+			<h1 class="site-title">SCHOOLWORK HELPSITE</h1>
+			<p class="site-description">The Essential Website for Learning</p>
+		</a>
+	</div>
